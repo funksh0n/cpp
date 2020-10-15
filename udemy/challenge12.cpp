d04c68043327b107f839e02a9682e39a85ce64f2
funksh0n <funksh0n@protonmail.ch>
2020-10-15 20:08:25 +0100

    consts

--- udemy/challenge12.cpp
+++ udemy/challenge12.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int *apply_all(int*, size_t, int*, size_t);
-void print(int*, size_t);
+int *apply_all(const int*, size_t, const int*, size_t);
+void print(const int*, size_t);
 
 void
-print(int* array, size_t size)
+print(const int* const array, size_t size)
 {
 	cout << "[ ";
 	for (size_t i {0}; i < size; i++)
@@ -14,7 +14,7 @@ print(int* array, size_t size)
 }
 
 int*
-apply_all(int* array1, size_t array1_size, int* array2, size_t array2_size)
+apply_all(const int* const array1, size_t array1_size, const int* const array2, size_t array2_size)
 {
 	size_t new_size = array1_size * array2_size;
 	int *new_array  = new int[new_size];
@@ -35,18 +35,21 @@ apply_all(int* array1, size_t array1_size, int* array2, size_t array2_size)
 int
 main(int argc, char** argv)
 {
+	const size_t array1_size {5};
+	const size_t array2_size {3};
+
 	int array1[] {1,2,3,4,5};
 	int array2[] {10,20,30};
 
 	cout << "Array 1: ";
-	print(array1, 5);
+	print(array1, array1_size);
 
 	cout << "Array 2: ";
-	print(array2, 3);
+	print(array2, array1_size);
 	
-	int *results = apply_all(array1, 5, array2, 3);
+	int *results = apply_all(array1, array1_size, array2, array2_size);
 	cout << "Result: ";
-	print(results, 15);
+	print(results, array1_size * array2_size);
 	delete[] results;
 
 	return EXIT_SUCCESS;
